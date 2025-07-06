26d66579c2aef431c356b29b1c540fb0daf688f7
sait han uzun <saithan.uzun@gmail.com>
2025-07-06 01:01:27 +0100

    blah blah

--- src/Frontend/Twitter.Frontend.BlazorApp/Infrastructure/Services/TweetService.cs
+++ src/Frontend/Twitter.Frontend.BlazorApp/Infrastructure/Services/TweetService.cs
@@ -1,9 +1,41 @@
+using Twitter.Frontend.BlazorApp.Infrastructure.Models.Dtos;
 using Twitter.Frontend.BlazorApp.Infrastructure.Models.Dvos;
 using Twitter.Frontend.BlazorApp.Infrastructure.Services.Interfaces;
 using Twitter.Frontend.BlazorApp.Layout;
+using Twitter.Frontend.BlazorApp.Pagination;
 
 namespace Twitter.Frontend.BlazorApp.Infrastructure.Services;
 
 public class TweetService : ITweetService
 {
+    public Task<List<TweetDvo>> GetTweets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TweetDvo> GetTweet(Guid tweetId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PagedViewModel<TweetDvo>> GetMainPageTweets(int page, int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PagedViewModel<TweetDvo>> GetProfilePageEntries(int page, int pageSize, string userName = null)
+    {
+        
+    }
+
+    public Task<PagedViewModel<TweetDvo>> GetEntryComments(Guid entryId, int page, int pageSize)
+    {
+        
+    }
+
+    public Task<Guid> CreateEntry(CreateTweetDto command)
+    {
+        throw new NotImplementedException();
+        
+    }
 }
